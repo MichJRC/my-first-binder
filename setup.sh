f78e48b03495ab7fda0ac8fd1123e7c7d796c0d9
MichJRC <michael.assouline@ec.europa.eu>
2025-07-18 14:10:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p data
-wget -q -O data/GSA2024LB.zip https://github.com/MichJRC/my-first-binder/releases/download/v1.0.0/GSA2024LB.zip
-cd data && unzip -q GSA2024LB.zip && cd ..
+mkdir -p downloaded_data
+wget -q -O downloaded_data/GSA2024LB.zip https://github.com/MichJRC/my-first-binder/releases/download/v1.0.0/GSA2024LB.zip
+cd downloaded_data && unzip -q GSA2024LB.zip && cd ..
 echo "Data setup complete!"
