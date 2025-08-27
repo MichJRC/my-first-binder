351fb540e8f62e252862dba700942f076951fd9a
MichJRC <michael.assouline@ec.europa.eu>
2025-08-27 16:05:24 +0200

    Delete exploring_netcdf.py

--- exploring_netcdf.py
+++
@@ -1,8 +0,0 @@
-import rasterio
-import numpy as np
-
-# Open the downloaded file
-with rasterio.open('c_gls_WB100_202506010000_GLOBE_S2_V1.0.1.nc') as src:
-    print("Shape:", src.shape)
-    print("CRS:", src.crs)
-    print("Bounds:", src.bounds)
